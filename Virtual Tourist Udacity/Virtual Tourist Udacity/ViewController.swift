93a3be8f81e65b6ca8103604402246eb3186058b
999lama <lama.meme113@gmail.com>
2021-09-21 17:04:27 +0300

    init commit

---
+++ Virtual Tourist Udacity/Virtual Tourist Udacity/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Virtual Tourist Udacity
+//
+//  Created by Lama Albadri on 21/09/2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
